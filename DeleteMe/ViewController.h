b6da3b83867025b85ad1da09c26ff80f1728acec
DeLightNight <asaffisher@icloud.com>
2014-06-14 23:59:25 +0300

    Initial Commit

---
+++ DeleteMe/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  DeleteMe
+//
+//  Created by Asaf Fisher on 6/14/14.
+//  Copyright (c) 2014 Asaf Fisher. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
